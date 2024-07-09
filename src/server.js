f82ec1865bab7a803c2765f3ac9e79d0ccc18f8b
CheerfulAdmiralAckbar <admiralackbar411@gmail.com>
2024-07-09 12:22:06 +0100

    feature: add Books model

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const Book = require("./books/model");
 
 const port = process.env.PORT || 5001;
 
@@ -8,10 +9,15 @@ const app = express();
 
 app.use(express.json());
 
+const syncTables = () => {
+  Book.sync();
+};
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "API is healthy" });
 });
 
 app.listen(port, () => {
+  syncTables();
   console.log(`Server is listening on port ${port}`);
 });
